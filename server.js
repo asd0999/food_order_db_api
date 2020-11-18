7fe4eb8fc5cc649eeede3d33fbe9d1b8b2cb5d0e
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-18 11:44:54 -0500

    add CORS

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 
 // CONFIGURATIONS
 const PORT = 3030;
@@ -11,20 +12,37 @@ mongoose.connection;
 
 // DATABASE
 mongoose.connect(
-    mongodbURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    },
-    () => {
-        console.log("the connection with mongod is established at", mongodbURI);
-    }
+  mongodbURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  () => {
+    console.log("the connection with mongod is established at", mongodbURI);
+  }
 );
+
+// CORS
+const whitelist = [
+  "http://localhost:3000",
+  "https://food-hubbs.herokuapp.com/",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 // MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(":method :url :status"));
+app.use(cors(corsOptions));
 
 // CONTROLLERS
 const restaurantController = require("./controllers/restaurantController");
@@ -35,10 +53,10 @@ app.use("/orders", orderController);
 
 // ROUTE
 app.get("/", (req, res) => {
-    res.send("index route working");
+  res.send("index route working");
 });
 
 // LISTENER
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening on port ${PORT}...`);
 });
