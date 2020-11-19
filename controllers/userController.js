af66dac55d3892fcb5f2d2f516f3ff2f5f65b65e
Jordan Abu-Sirriya <jordan.abu23@gmail.com>
2020-11-19 14:11:38 -0600

    updating models

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const User = require("../models/userModel")
-const Order = require("../models/orderModel");
 const users = express.Router();
 
 //DATA
@@ -16,8 +15,15 @@ const newUsers = require("../dataFiles/userData")
 
 
 //USER  SEED ROUTES
-users.get("/seed", (req, res) => {
-    console.log(newUsers)
+users.get("/seed/seed", (req, res) => {
+    console.log(newUsers);
+    User.create(newUsers, (err, createdUsers) => {
+
+        if (err) {
+            console.log(err);
+        }
+        console.log
+    })
 })
 
 
