354fa26bec3210837ab616017665093fc1b1148b
asd0999 <obelixotic@gmail.com>
2020-11-20 09:42:20 -0500

    comma

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
     user_id: { type: String },
-    delivery: { type: Boolean, default: true }
+    delivery: { type: Boolean, default: true },
     order: [{
         item_id: { type: Schema.Types.ObjectId, ref: "MenuItem" },
         quantity: { type: Number },
