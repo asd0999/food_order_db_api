b4098dafa9b81c66767bb2fcee62fedb161f6a5c
asd0999 <obelixotic@gmail.com>
2020-11-21 15:08:44 -0500

    place order working, changes made to order schema

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
     user_id: { type: String },
+    user_name: { type: String },
     delivery: { type: Boolean, default: true },
-    order: [{
-        item_id: { Type: String },
+    itemsInOrder: [{
+        item_id: { type: String },
         quantity: { type: Number, default: 1 },
     }, ],
 }, {
--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,13 @@
-
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
+    username: { type: String, required: true },
+    password: { type: String, required: true },
     email: { type: String },
     phoneNumber: { type: Number },
-    order: [{
-        type: Schema.Types.ObjectId,
-        ref: "Order",
-    }, ],
-})
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
